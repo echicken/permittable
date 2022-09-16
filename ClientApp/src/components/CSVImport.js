98b39d2fc767a26acab869d54a12bfc862df42b3
echicken <echicken@bbs.electronicchicken.com>
2022-09-16 00:14:41 -0400

    Better date handling.

--- ClientApp/src/components/CSVImport.js
+++ ClientApp/src/components/CSVImport.js
@@ -35,7 +35,7 @@ const CSVImport = props => {
 	const parseDate = d => {
 		const m = d.match(/^(\d{4})(\d{2})(\d{2})\d{6}$/); // Newer CSVs have YYYYMMDD000000
 		if (m !== null) d = `${m[1]}/${m[2]}/${m[3]}`; // Older CSVs have YYYY/MM/DD
-		return d ? new Date(d) : null;
+		new Date(d || 0);
 	}
 
 	const formatPermit = permit => {
