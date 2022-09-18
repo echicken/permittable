219484121d074746f43820561a51d96ff5843a70
echicken <echicken@bbs.electronicchicken.com>
2022-09-18 00:06:12 -0400

    Use historic streetview panoramas if available. Added a range control under the map element to allow scrubbing through available panoramas. On mouseover of a permit in the list, switch to the panorama with the date closest to the issue date of that permit.

--- ClientApp/src/components/Address.js
+++ ClientApp/src/components/Address.js
@@ -1,176 +1,191 @@
 import React, { useState } from 'react';
 import { Link, useParams, useLocation } from 'react-router-dom';
-import { Table } from 'reactstrap';
+import { Table, Input } from 'reactstrap';
 import { Marker, StreetViewPanorama, StreetViewService } from '@react-google-maps/api';
 import Map from './Map';
 
 const Sorter = props => {
 
-    const sort = dir => {
-        const s = props.data.slice(0).sort((a, b) => {
-            if (a[props.sortBy] < b[props.sortBy]) {
-                return dir === 'ascending' ? -1 : 1;
-            }
-            if (a[props.sortBy] > b[props.sortBy]) {
-                return dir === 'ascending' ? 1: -1;
-            }
-            return 0;
-        });
-        props.setter(s);
-    }
+	const sort = dir => {
+		const s = props.data.slice(0).sort((a, b) => {
+			if (a[props.sortBy] < b[props.sortBy]) {
+				return dir === 'ascending' ? -1 : 1;
+			}
+			if (a[props.sortBy] > b[props.sortBy]) {
+				return dir === 'ascending' ? 1: -1;
+			}
+			return 0;
+		});
+		props.setter(s);
+	}
 
-    return (
-        <>
-            <span className="sort-button" onClick={() => sort('ascending')}>▲</span>
-            <span className="sort-button" onClick={() => sort('descending')}>▼</span>
-        </>
-    );
+	return (<>
+		<span className="sort-button" onClick={() => sort('ascending')}>▲</span>
+		<span className="sort-button" onClick={() => sort('descending')}>▼</span>
+	</>);
+
 }
 
 const PermitListItem = props => {
-    return (
-        <tr>
-            <td>
-                <Link to={`/view-permit/${props.permit.Number}/${props.permit.Revision}`} state={props.permit}>
-                    {props.permit.Number}
-                </Link>
-            </td>
-            <td>{props.permit.Revision}</td>
-            <td>{props.permit.PermitType}</td>
-            <td>{props.permit.Issued}</td>
-            <td>{props.permit.Completed}</td>
-            <td>{props.permit.ShortDescription}</td>
-        </tr>
-    );
+	return (
+		<tr onMouseOver={() => props.onHover(props.permit)}>
+			<td>
+				<Link to={`/view-permit/${props.permit.Number}/${props.permit.Revision}`} state={props.permit}>
+					{props.permit.Number}
+				</Link>
+			</td>
+			<td>{props.permit.Revision}</td>
+			<td>{props.permit.PermitType}</td>
+			<td>{props.permit.Issued}</td>
+			<td>{props.permit.Completed}</td>
+			<td>{props.permit.ShortDescription}</td>
+		</tr>
+	);
 }
 
 const PermitList = props => {
-    const list = props.data.map(e => <PermitListItem key={`${e.Number},${e.Revision}`} permit={e} />);
-    return <>{list}</>
+	const list = props.data.map(e => <PermitListItem key={`${e.Number},${e.Revision}`} permit={e} onHover={props.onPermitHover} />);
+	return <>{list}</>;
+}
+
+const PermitTable = props => {
+	return (<>
+		<h2>Permits for {props.address.Text}</h2>
+		<Table hover responsive>
+			<thead>
+				<tr>
+					<th>Number <Sorter data={props.permits} sortBy="Number" setter={props.setPermits} /></th>
+					<th>Revision <Sorter data={props.permits} sortBy="Revision" setter={props.setPermits} /></th>
+					<th>Type <Sorter data={props.permits} sortBy="PermitType" setter={props.setPermits} /></th>
+					<th>Issued <Sorter data={props.permits} sortBy="Issued" setter={props.setPermits} /></th>
+					<th>Completed <Sorter data={props.permits} sortBy="Completed" setter={props.setPermits} /></th>
+					<th>Description <Sorter data={props.permits} sortBy="ShortDescription" setter={props.setPermits} /></th>
+				</tr>
+			</thead>
+			<tbody>
+				<PermitList data={props.permits} onPermitHover={props.onPermitHover} />
+			</tbody>
+		</Table>
+	</>);
+}
+
+const PanoRanger = props => {
+	if (!props.panos || props.panos.length < 2) return;
+	return (
+		<Input
+			type="range"
+			name="range"
+			min="0"
+			max={props.panos.length - 1}
+			value={props.panoIdx}
+			step={1}
+			onChange={evt => props.setPanoIdx(evt.target.value)}
+		/>
+	);
 }
 
 const Address = () => {
 
-    const [ permits, setPermits ] = useState(null);
-    const [ loadingPermits, setLoadingPermits ] = useState(false);
-    const [ address, setAddress ] = useState(null);
-    const [ loadingAddress, setLoadingAddress ] = useState(false);
-    const [ pano, setPano ] = useState(undefined);
-    const [ panos, setPanos ] = useState([]);
+	const [ permits, setPermits ] = useState(null);
+	const [ loadingPermits, setLoadingPermits ] = useState(false);
+	const [ address, setAddress ] = useState(null);
+	const [ loadingAddress, setLoadingAddress ] = useState(false);
+	const [ panoIdx, setPanoIdx ] = useState(0);
+	const [ panos, setPanos ] = useState([]);
 
-    const { geoid } = useParams();
-    const location = useLocation();
+	const { geoid } = useParams();
+	const location = useLocation();
 
-    const fetchPermits = async () => {
-        setLoadingPermits(true);
-        try {
-            const response = await fetch(`/api/permit/address/${geoid}/permits`, { credentials: 'same-origin' });
-            const data = await response.json();
-            if (data.length) {
-                const p = data.sort((a, b) => a.Issued > b.Issued ? -1 : 1);
-                setPermits(p);
-            }
-        } catch (err) {
-            console.log('Error fetching address data', err);
-        }
-        setLoadingPermits(false);
-    }
+	const fetchPermits = async () => {
+		setLoadingPermits(true);
+		try {
+			const response = await fetch(`/api/permit/address/${geoid}/permits`, { credentials: 'same-origin' });
+			const data = await response.json();
+			if (data.length) {
+				const p = data.sort((a, b) => a.Issued > b.Issued ? -1 : 1);
+				setPermits(p);
+			}
+		} catch (err) {
+			console.log('Error fetching address data', err);
+		}
+		setLoadingPermits(false);
+	}
 
-    const fetchAddress = async () => {
-        setLoadingAddress(true);
-        try {
-            const response = await fetch(`/api/address/by-id/${geoid}`, { credentials: 'same-origin' });
-            const data = await response.json();
-            setAddress(data);
-        } catch (err) {
-            console.log('Error fetching address data', err);
-        }
-        setLoadingAddress(false);
-    }
+	const fetchAddress = async () => {
+		setLoadingAddress(true);
+		try {
+			const response = await fetch(`/api/address/by-id/${geoid}`, { credentials: 'same-origin' });
+			const data = await response.json();
+			setAddress(data);
+		} catch (err) {
+			console.log('Error fetching address data', err);
+		}
+		setLoadingAddress(false);
+	}
 
-    const onPanorama = panorama => {
-        panorama.addListener('links_changed', () => {
-            const _pos = panorama.getPosition();
-            const pos = { lat: _pos.lat(), lng: _pos.lng() };
-            const heading = window.google.maps.geometry.spherical.computeHeading(pos, center);
-            panorama.setPov({
-                heading: heading,
-                pitch: 0,
-                zoom: 1,
-            });
-        });
-    }
+	const onPanorama = panorama => {
+		panorama.addListener('links_changed', () => {
+			const _pos = panorama.getPosition();
+			const pos = { lat: _pos.lat(), lng: _pos.lng() };
+			const heading = window.google.maps.geometry.spherical.computeHeading(pos, center);
+			panorama.setPov({
+				heading: heading,
+				pitch: 0,
+				zoom: 1,
+			});
+		});
+	}
 
-    if (permits === null) {
-        if (!loadingPermits) fetchPermits();
-        return <h2>Loading ...</h2>;
-    }
+	const onService = svs => {
+		svs.getPanorama({ location: center, radius: 50 }, (data, status) => {
+			if (!data) return;
+			if (status !== 'OK') return;
+			setPanos(data.time);
+			setPanoIdx(data.time.length - 1);
+		});
+	}
 
-    if (address === null) {
-        if (location.state?.address !== undefined) {
-            setAddress(location.state.address);
-            return;
-        } else {
-            if (!loadingAddress) fetchAddress();
-            return <h2>Loading ...</h2>
-        }
-    }
+	const onPermitHover = permit => {
+		const pid = new Date(permit.Issued);
+		let closestDiff = Infinity;
+		let closestPano = undefined;
+		for (let i = 0; i < panos.length; i++) {
+			const diff = Math.abs(panos[i].Jo - pid);
+			if (diff > closestDiff) continue;
+			closestDiff = diff;
+			closestPano = i;
+		}
+		setPanoIdx(closestPano);
+	}
 
-    if (!permits.length) {
-        return <h2>No permits available for this address.</h2>
-    }
+	if (permits === null) {
+		if (!loadingPermits) fetchPermits();
+		return <h2>Loading ...</h2>;
+	}
 
-    const center = { lat: address.Latitude, lng: address.Longitude };
+	if (address === null) {
+		if (location.state?.address !== undefined) {
+			setAddress(location.state.address);
+			return;
+		} else {
+			if (!loadingAddress) fetchAddress();
+			return <h2>Loading ...</h2>
+		}
+	}
 
-    const onService = svs => {
-        svs.getPanorama(
-            { location: center, radius: 50 },
-            (data, status) => {
-                if (status !== 'OK') return;
-                console.debug(data.time);
-                setPanos(data.time);
+	if (!permits.length) return <h2>No permits available for this address.</h2>;
 
-                // On mouseover of item in PermitList, find best match in 'panos' (panos[n].Jo) for the issue(?) date of the permit
-                // and then update 'pano' (setPano) to panos[n].pano to display that historical image.
+	const center = { lat: address.Latitude, lng: address.Longitude };
 
-                // This will do a slideshow of all available panos for our location:
-
-                // let n = 0;
-                // setInterval(() => {
-                //     setPano(data.time[n].pano);
-                //     n++;
-                //     if (n >= data.time.length) n = 0;
-                // }, 5000);
-
-            }
-        );
-    }
-
-    return (
-        <>
-            <Map center={center} zoom={11}>
-                <Marker position={center} />
-                <StreetViewPanorama options={{ imageDateControl: true }} pano={pano} position={center} visible={true} onLoad={onPanorama} />
-                <StreetViewService onLoad={onService} />
-            </Map>
-            <h2>Permits for {address.Text}</h2>
-            <Table hover responsive>
-                <thead>
-                    <tr>
-                        <th>Number <Sorter data={permits} sortBy="Number" setter={setPermits} /></th>
-                        <th>Revision <Sorter data={permits} sortBy="Revision" setter={setPermits} /></th>
-                        <th>Type <Sorter data={permits} sortBy="PermitType" setter={setPermits} /></th>
-                        <th>Issued <Sorter data={permits} sortBy="Issued" setter={setPermits} /></th>
-                        <th>Completed <Sorter data={permits} sortBy="Completed" setter={setPermits} /></th>
-                        <th>Description <Sorter data={permits} sortBy="ShortDescription" setter={setPermits} /></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <PermitList data={permits} />
-                </tbody>
-            </Table>
-        </>
-    );
+	return (<>
+		<Map center={center} zoom={11}>
+			<Marker position={center} />
+			<StreetViewPanorama options={{ imageDateControl: true }} pano={panos[panoIdx]?.pano} position={center} visible={true} onLoad={onPanorama} />
+			<StreetViewService onLoad={onService} />
+		</Map>
+		<PanoRanger panos={panos} panoIdx={panoIdx} setPanoIdx={setPanoIdx} />
+		<PermitTable address={address} permits={permits} setPermits={setPermits} onPermitHover={onPermitHover} />
+	</>);
 
 }
 
