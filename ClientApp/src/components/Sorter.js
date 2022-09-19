f4611f317262529c3ef67dea036621dccb5a4769
echicken <echicken@bbs.electronicchicken.com>
2022-09-18 23:43:07 -0400

    Clearspace

--- ClientApp/src/components/Sorter.js
+++ ClientApp/src/components/Sorter.js
@@ -7,27 +7,27 @@ const Sorter = props => {
 
 	const sort = (dir, data, key) => {
 		const s = data.sort((a, b) => {
-            if (dir === ASCENDING) {
-                if (a[key] < b[key]) {
-                    return -1;
-                } else if (a[key] > b[key]) {
-                    return 1;
-                }
-                return 0;
-            }
-            if (a[key] < b[key]) {
-                return 1;
-            } else if (a[key] > b[key]) {
-                return -1;
-            }
-            return 0;
+			if (dir === ASCENDING) {
+				if (a[key] < b[key]) {
+					return -1;
+				} else if (a[key] > b[key]) {
+					return 1;
+				}
+				return 0;
+			}
+			if (a[key] < b[key]) {
+				return 1;
+			} else if (a[key] > b[key]) {
+				return -1;
+			}
+			return 0;
 		});
 		props.setter(s);
 	}
 
-    const oc = evt => {
-        (props.sortFunction || sort)(Symbol.for(evt.target.dataset.sortdir), props.data.slice(0), props.sortBy);
-    }
+	const oc = evt => {
+		(props.sortFunction || sort)(Symbol.for(evt.target.dataset.sortdir), props.data.slice(0), props.sortBy);
+	}
 
 	return (<>
 		<span data-sortdir={Symbol.keyFor(ASCENDING)} className="sort-button" onClick={oc}>▲</span>
